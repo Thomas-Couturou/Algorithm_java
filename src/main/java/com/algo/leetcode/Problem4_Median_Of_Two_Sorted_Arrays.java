1c1f6b9304315ff6c01e84986e1724c247f61066
Thomas-Couturou <thomas.couturou@eleves.Ec-nantes.fr>
2025-03-05 14:47:57 +0100

    Fix : Rename Problem4 with a Capital Letter

--- src/main/java/com/algo/leetcode/Problem4_Median_Of_Two_Sorted_Arrays.java
+++ src/main/java/com/algo/leetcode/Problem4_Median_Of_Two_Sorted_Arrays.java
@@ -1,6 +1,6 @@
 package com.algo.leetcode;
 
-public class problem4_Median_Of_Two_Sorted_Arrays {
+public class Problem4_Median_Of_Two_Sorted_Arrays {
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
--- src/test/java/com/algo/leetcode/TestProblem4.java
+++ src/test/java/com/algo/leetcode/TestProblem4.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestProblem4 {
     @Test
     public void testProblem4(){
-        assertEquals(problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 2.0);
-        assertEquals(problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 2.5);
-        assertEquals(problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{}, new int[]{1}), 1.0);
-        assertEquals(problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{}, new int[]{}), 0.0);
+        assertEquals(Problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 2.0);
+        assertEquals(Problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 2.5);
+        assertEquals(Problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{}, new int[]{1}), 1.0);
+        assertEquals(Problem4_Median_Of_Two_Sorted_Arrays.findMedianSortedArrays(new int[]{}, new int[]{}), 0.0);
     }
 }
